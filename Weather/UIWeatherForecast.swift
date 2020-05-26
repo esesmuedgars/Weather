bcec96287b9c6be499f153b811a1196e809d0950
Edgars Vanags <edgars.vanags1@gmail.com>
2020-05-26 13:23:56 +0300

    Added UIWeatherForecast use case

---
+++ Weather/UIWeatherForecast.swift
@@ -0,0 +1,49 @@
+//
+// Copyright © 2020 @esesmuedgars.
+//
+
+import UIKit
+
+struct UIWeatherForecast {
+    let description: String
+    let icon: Icon
+    let temperature: String
+    
+    enum Icon: String {
+        case clearDay = "clear-day"
+        case clearNight = "clear-night"
+        case rain
+        case snow
+        case sleet
+        case wind
+        case fog
+        case cloudy
+        case partlyCloudyDay = "partly-cloudy-day"
+        case partlyCloudyNight = "partly-cloudy-night"
+        
+        var image: UIImage? {
+            switch self {
+            case .clearDay:
+                return UIImage(systemName: "sun.max")
+            case .clearNight:
+                return UIImage(systemName: "moon.stars")
+            case .rain:
+                return UIImage(systemName: "cloud.rain")
+            case .snow:
+                return UIImage(systemName: "cloud.snow")
+            case .sleet:
+                return UIImage(systemName: "cloud.sleet")
+            case .wind:
+                return UIImage(systemName: "wind")
+            case .fog:
+                return UIImage(systemName: "cloud.fog")
+            case .cloudy:
+                return UIImage(systemName: "cloud")
+            case .partlyCloudyDay:
+                return UIImage(systemName: "cloud.sun")
+            case .partlyCloudyNight:
+                return UIImage(systemName: "cloud.moon")
+            }
+        }
+    }
+}
